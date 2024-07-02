be3e8bea8591166044211fe17513df925a01c19d
Husam Hammad <85282854+husamhammad@users.noreply.github.com>
2024-07-02 17:33:38 +0300

    feat:Create Setup and Requirements Files (#1)

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in kard_theme/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('newara/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
+
+setup(
+	name='newara',
+	version=version,
+	description='Custom Frappe Theme',
+	author='husamhammad',
+	author_email='husamhammad542@gmail.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
